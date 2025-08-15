7c88206d662d745b5ef632859704eb62013f7717
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2025-08-16 03:04:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Routes
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
+    
